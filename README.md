475b5281b3410e5ed68724d5306b624a2cdc2505
Vitor George <vitor.george@gmail.com>
2018-10-29 12:26:35 +0000

    Use config module instead of knexfile

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 Data service web server for Global Electrification Platform.
 
+## Development 
+
+### Testing
+
+Start and populate testing database:
+
+  yarn init-test-db
+
+Start and populate testing database:
+
+  yarn migrate-test-db
+
+Run tests:
+
+  yarn test
+
 ## License
 
 [MIT](LICENSE)
--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,2 @@
-module.exports = {
-  client: "postgresql",
-  connection: {
-    database: "gep-data-service-test",
-    user: "gep-data-service-test",
-    host: "localhost",
-    port: "15433"
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: "knex_migrations"
-  }
-};
+const config = require("config");
+module.exports = config.get("knex");
