1657bddc973761a7bf1f1a6c5e9fb18fb4851dbb
Vitor George <vitor.george@gmail.com>
2018-12-04 17:51:45 +0000

    Update seeding

--- seeds/seed-scenarios.js
+++ seeds/seed-scenarios.js
@@ -1,59 +1,82 @@
-const { readdirSync } = require('fs');
+const { readFile, readdir } = require('fs-extra');
 const { join } = require('path');
 const csv = require('fast-csv');
+const yaml = require('js-yaml');
+const path = require('path');
 
-const scenariosDirPath = join(__dirname, 'fixtures', 'scenarios');
-
-async function readScenariosFile (knex, scenarioFileName) {
-  const [scenarioId] = scenarioFileName.split('.');
-  const scenarioFilePath = join(scenariosDirPath, scenarioFileName);
-
-  // Technologies, by index, on mw-1 scenarios
-  const techs = [
-    'grid',
-    'sa-diesel',
-    'sa-pv',
-    'mg-diesel',
-    'mg-pv',
-    'mg-wind',
-    'mg-hydro'
-  ];
-
-  return new Promise(function (resolve, reject) {
-    const entries = [];
-
-    csv
-      .fromPath(scenarioFilePath, { headers: true, delimiter: ';' })
-      .on('data', results => {
-        // Convert columns to object properties
-        const entry = {
-          scenarioId: scenarioId,
-          areaId: results.ID,
-          electrificationTech: techs[results.FinalElecCode2030],
-          investmentCost: parseFloat(results.InvestmentCost2030),
-          newCapacity: parseFloat(results.NewCapacity2030),
-          electrifiedPopulation: parseFloat(results.Pop)
-        };
-        delete results.ID;
-        delete results.FinalElecCode2030;
-        delete results.InvestmentCost2030;
-        delete results.NewCapacity2030;
-        delete results.Pop;
-        entry.results = results;
-        entries.push(entry);
-      })
-      .on('end', async () => {
-        // Insert to the database
-        await knex('scenarios')
-          .insert(entries)
-          .then(resolve);
-      })
-      .on('error', reject);
-  });
-}
+const scenariosPath = join(__dirname, 'fixtures', 'scenarios');
+const modelsPath = join(__dirname, 'fixtures', 'models');
 
 exports.seed = async function (knex, Promise) {
+  function getModelId (scenarioId) {
+    return scenarioId.substring(0, scenarioId.lastIndexOf('-'));
+  }
+
+  async function loadModelFromFile (modelId) {
+    const modelYaml = await readFile(
+      path.join(modelsPath, `${modelId}.yml`),
+      'utf-8'
+    );
+    return yaml.load(modelYaml);
+  }
+
+  async function getFilters (scenarioId) {
+    const modelId = getModelId(scenarioId);
+    const model = await loadModelFromFile(modelId);
+    return model.filters.map(filter => {
+      return { property: filter.property, type: filter.type };
+    });
+  }
+
+  async function readScenariosFile (scenarioFileName) {
+    const [scenarioId] = scenarioFileName.split('.');
+    const scenarioFilePath = join(scenariosPath, scenarioFileName);
+
+    const filters = await getFilters(scenarioId);
+
+    // Read CSV File
+    return new Promise(function (resolve, reject) {
+      const entries = [];
+
+      csv
+        .fromPath(scenarioFilePath, { headers: true, delimiter: ';' })
+        .on('data', record => {
+          // Convert columns to object properties
+          const entry = {
+            scenarioId: scenarioId,
+            areaId: record.ID,
+            electrificationTech: record.FinalElecCode2030,
+            investmentCost: parseFloat(record.InvestmentCost2030),
+            newCapacity: parseFloat(record.NewCapacity2030),
+            electrifiedPopulation: parseFloat(record.Pop),
+            filterValues: {}
+          };
+
+          // Ingest values to be filtered
+          for (const filter of filters) {
+            entry.filterValues[filter.property] = record[filter.property];
+
+            // Convert to number if filter is type of range
+            if (filter.type === 'range') {
+              entry.filterValues[filter.property] = parseFloat(
+                entry.filterValues[filter.property]
+              );
+            }
+          }
+
+          entries.push(entry);
+        })
+        .on('end', async () => {
+          // Insert to the database
+          await knex('scenarios')
+            .insert(entries)
+            .then(resolve);
+        })
+        .on('error', reject);
+    });
+  }
+
   await knex('scenarios').del();
-  const scenarioFiles = readdirSync(scenariosDirPath);
-  return Promise.all(scenarioFiles.map(file => readScenariosFile(knex, file)));
+  const scenarioFiles = await readdir(scenariosPath);
+  return Promise.all(scenarioFiles.map(file => readScenariosFile(file)));
 };
