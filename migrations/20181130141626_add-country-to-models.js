52e4aa36fb7a4fc2b103e8fbafe88ecd34c01a41
Vitor George <vitor.george@gmail.com>
2018-12-04 17:48:29 +0000

    Change "results" column to "filterValues"

--- migrations/20181130141626_add-country-to-models.js
+++ migrations/20181130141626_add-country-to-models.js
@@ -1,10 +1,10 @@
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
   return knex.schema.alterTable('models', function (t) {
     t.string('country', 2);
   });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
   return knex.schema.alterTable('models', function (t) {
     t.dropColumn('country');
   });
