5bb816f851645447b9b7ed31dcfcfe981792aa65
Vitor George <vitor.george@gmail.com>
2019-08-22 11:38:30 +0100

    Set model's "baseYear" not nullable and fix migration rollback.

--- migrations/20190819123220_add-model-base-year.js
+++ migrations/20190819123220_add-model-base-year.js
@@ -1,11 +1,11 @@
 exports.up = function (knex, Promise) {
   return knex.schema.table('models', function (t) {
-    t.integer('baseYear');
+    t.integer('baseYear').notNullable();
   });
 };
 
 exports.down = function (knex, Promise) {
   return knex.schema.alterTable('models', function (t) {
-    t.integer('baseYear');
+    t.dropColumn('baseYear');
   });
 };
