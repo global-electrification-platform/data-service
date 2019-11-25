904db72ba6e06408ee6b3ca67530fdba21353d5a
Vitor George <vitor.george@gmail.com>
2019-11-25 12:22:13 +0000

    If cache is enabled, run scenarios query asynchronously.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 const config = require('config');
-const _ = require('lodash');
-const qs = require('qs');
 const boom = require('boom');
 const Hapi = require('@hapi/hapi');
 const Joi = require('joi');
-const { delay } = require('./utils');
-
 const db = require('./db');
+const { delay } = require('./utils');
+const getScenario = require('./services/get-scenario');
+
 const riseIndicatorsData = require('./rise-indicators.json');
 
 // Get Redis config
@@ -260,306 +259,53 @@ server.route({
   handler: async function (request) {
     try {
       const id = request.params.id.toLowerCase();
-      const modelId = id.substring(0, id.lastIndexOf('-'));
-      let filters;
+      const { query } = request;
 
-      // Parse query string if available
-      let { query } = request;
-      let targetYear = null;
+      // If cache is not enabled, run query synchronously and return results
+      if (!cache.enabled) {
+        return await getScenario(id, query);
+      }
 
       // Generate cache key from query params
       const cacheKey = JSON.stringify({ id, query });
-      if (cache.enabled) {
-        // Load cache entry
-        let cachedData = await redisClient.getObject(cacheKey);
 
-        // If cache entry exists
-        if (cachedData) {
-          // Check every second if query is running. As database queries can
-          // take a while, this avoids starting the same query in parallel.
-          let duration = 0;
-          let step = 1000;
-          while (cachedData.runningDbQuery) {
-            // If query is running for too long, abort cache load and throw
-            // error.
-            if (duration > cache.loadTimeout) {
-              return boom.gatewayTimeout();
-            }
+      // Load cache entry
+      let cachedData = await redisClient.getObject(cacheKey);
 
-            // Wait
-            await delay(step);
-
-            // Update query duration time
-            duration = duration + step;
-
-            // Get cached data
-            cachedData = await redisClient.getObject(cacheKey);
-          }
-
-          // Renew cache expiration time
-          await redisClient.expire(cacheKey, cacheTtl);
-
-          // Parse string into JSON
-          return cachedData;
-        }
-
-        // No cache data is available, flag runningDbQuery as true
-        await redisClient.setObject(
-          cacheKey,
-          { runningDbQuery: true },
-          cache.loadTimeout
-        );
+      // If no cache entry exists, start query in "background"
+      if (!cachedData) {
+        getScenario(id, query);
       }
 
-      if (query) {
-        query = qs.parse(query);
-        targetYear = parseInt(query.year) || null;
-
-        // Validate and parse filters
-        filters = query.filters;
-        if (filters) {
-          // Filters must be in an Array
-          if (!Array.isArray(filters)) {
-            throw new SyntaxError('Filters must be an Array.');
-          }
-
-          // Validate range values
-          filters.forEach(filter => {
-            // A filter key must be defined
-            if (typeof filter.key === 'undefined') {
-              throw new SyntaxError('Filter must include "key".');
-            }
-
-            if (
-              typeof filter.min === 'undefined' &&
-              typeof filter.max === 'undefined' &&
-              typeof filter.options === 'undefined'
-            ) {
-              throw new SyntaxError(
-                'Filter must include a valid value parameter name: "min", "max" or "options").'
-              );
-            }
-          });
-        }
+      // If there is no query lock, return cached results
+      if (cachedData && !cachedData.runningDbQuery) {
+        // Renew cache expiration time
+        redisClient.expire(cacheKey, cacheTtl);
+        return cachedData;
       }
 
-      // Get information about the model
-      const model = await db('models')
-        .select('filters', 'timesteps', 'baseYear')
-        .where('id', modelId)
-        .first();
+      // Query is running, start observing cache key for query completion
+      let requestDuration = 0;
+      const cacheCheckInterval = 1000; // 1 s
+      do {
+        // Wait 1 second
+        await delay(cacheCheckInterval);
+        requestDuration = requestDuration + cacheCheckInterval;
 
-      const { baseYear } = model;
-      let [intermediateYear, finalYear] = model.timesteps;
-
-      // Validate timestep model
-      if (model.timesteps) {
-        // Set the year to the 1st value if null
-        targetYear = targetYear || model.timesteps[0];
-        if (model.timesteps.indexOf(targetYear) === -1) {
-          throw new SyntaxError(
-            `The "year" parameter [${targetYear}] is invalid for this scenario. Must be one of [${model.timesteps.join(
-              ', '
-            )}]`
-          );
-        }
-      } else {
-        // Disregard year
-        targetYear = '';
-      }
-
-      // Get steps before the target year. This is used to calculate accumulated
-      // investment costs
-      const includedSteps = model.timesteps.filter(y => y <= targetYear);
-      const investmentCostSelector = includedSteps
-        .map(year => {
-          return `(summary->>'InvestmentCost${year}')::numeric`;
-        })
-        .join(' + ');
-
-      // Assemble summary keys based on year numbers
-      const summaryKeys = {
-        popBaseYear: 'Pop' + baseYear,
-        popIntermediateYear: 'Pop' + intermediateYear,
-        popFinalYear: 'Pop' + finalYear,
-        popConnectedBaseYear: 'PopConnected' + baseYear,
-
-        elecStatusIntermediateYear: 'ElecStatusIn' + intermediateYear,
-        elecStatusFinalYear: 'ElecStatusIn' + finalYear,
-
-        elecTypeBaseYear: 'ElecCode' + baseYear,
-        elecTypeIntermediateYear: 'FinalElecCode' + intermediateYear,
-        elecTypeFinalYear: 'FinalElecCode' + finalYear,
-
-        electrificationTech: 'FinalElecCode' + targetYear,
-        newCapacity: 'NewCapacity' + targetYear,
-        electrificationStatus: 'ElecStatusIn' + targetYear
-      };
-
-      const whereBuilder = builder => {
-        builder.where('scenarioId', id);
-
-        if (filters) {
-          filters.forEach(filter => {
-            const { min, max, options } = filter;
-            let { key } = filter;
-
-            const filterDef = model.filters.find(f => f.key === key);
-            // Update key if is a timestep filter
-            key = filterDef.timestep ? key + targetYear : key;
-
-            if (typeof min !== 'undefined') {
-              builder.whereRaw(`("filterValues"->>?)::numeric >= ?`, [
-                key,
-                parseFloat(min)
-              ]);
-            }
-
-            if (typeof max !== 'undefined') {
-              builder.whereRaw(`("filterValues"->>?)::numeric <= ?`, [
-                key,
-                parseFloat(max)
-              ]);
-            }
-
-            if (Array.isArray(options) && options.length > 0) {
-              builder.whereRaw(
-                `("filterValues"->>?::text) in (${options
-                  .map(_ => '?')
-                  .join(',')})`,
-                [key, ...options]
-              );
-            }
-          });
-        }
-      };
-
-      // Get summary for filtered clusters
-      const summary = await db
-        .select(
-          db.raw(`
-            SUM(
-              (summary->>'${summaryKeys.popBaseYear}')::numeric
-            ) as "popBaseYear",
-            SUM(
-              (summary->>'${summaryKeys.popIntermediateYear}')::numeric
-            ) as "popIntermediateYear",
-            SUM(
-              (summary->>'${summaryKeys.popFinalYear}')::numeric
-              ) as "popFinalYear"
-          `),
-          db.raw(`
-            SUM(${investmentCostSelector}) as "investmentCost"
-          `),
-          db.raw(`
-            SUM(
-              (summary->>'${summaryKeys.newCapacity}')::numeric
-            ) as "newCapacity"
-          `)
-        )
-        .first()
-        .where(whereBuilder)
-        .from('scenarios');
-
-      // Parse query results into a 2 decimals number
-      Object.keys(summary).forEach(key => {
-        summary[key] = _.round(summary[key], 2);
-      });
-
-      // Get features
-      const features = await db
-        .select(
-          'featureId as id',
-          db.raw(
-            `summary->>'${summaryKeys.popConnectedBaseYear}' as "popConnectedBaseYear"`
-          ),
-          db.raw(
-            `(summary->>'${summaryKeys.popIntermediateYear}')::numeric * (summary->>'${summaryKeys.elecStatusIntermediateYear}')::numeric as "popConnectedIntermediateYear"`
-          ),
-          db.raw(
-            `(summary->>'${summaryKeys.popFinalYear}')::numeric * (summary->>'${summaryKeys.elecStatusFinalYear}')::numeric as "popConnectedFinalYear"`
-          ),
-          db.raw(
-            `summary->>'${summaryKeys.elecTypeBaseYear}' as "elecTypeBaseYear"`
-          ),
-          db.raw(
-            `summary->>'${summaryKeys.elecTypeIntermediateYear}' as "elecTypeIntermediateYear"`
-          ),
-          db.raw(
-            `summary->>'${summaryKeys.elecTypeFinalYear}' as "elecTypeFinalYear"`
-          ),
-          db.raw(
-            `summary->>'${summaryKeys.electrificationTech}' as "electrificationTech"`
-          ),
-          db.raw(
-            `(${investmentCostSelector}) as "investmentCost"`
-          ),
-          db.raw(`summary->>'${summaryKeys.newCapacity}' as "newCapacity"`),
-          db.raw(
-            `summary->>'${summaryKeys.electrificationStatus}' as "electrificationStatus"`
-          )
-        )
-        .where(whereBuilder)
-        .orderBy('featureId')
-        .from('scenarios');
-
-      const summaryByType = {
-        popConnectedBaseYear: {},
-        popConnectedIntermediateYear: {},
-        popConnectedFinalYear: {},
-        investmentCost: {},
-        newCapacity: {}
-      };
-
-      // Organize features into layers and calculate summary by type
-      let featureTypes = [];
-      for (const f of features) {
-        featureTypes[f.id] = f.electrificationTech;
-
-        // Base year, discard null electrification type
-        if (f.elecTypeBaseYear) {
-          summaryByType.popConnectedBaseYear[f.elecTypeBaseYear] =
-            (summaryByType.popConnectedBaseYear[f.elecTypeBaseYear] || 0) +
-            parseFloat(f.popConnectedBaseYear);
+        // Check for load timeout
+        if (requestDuration > cache.loadTimeout) {
+          return boom.gatewayTimeout();
         }
 
-        // Intermediate year
-        if (f.elecTypeIntermediateYear) {
-          summaryByType.popConnectedIntermediateYear[f.elecTypeIntermediateYear] =
-            (summaryByType.popConnectedIntermediateYear[f.elecTypeIntermediateYear] ||
-              0) + parseFloat(f.popConnectedIntermediateYear);
-        }
+        // Get cached data
+        cachedData = await redisClient.getObject(cacheKey);
+      } while (cachedData.runningDbQuery);
 
-        // Final year
-        if (f.elecTypeFinalYear) {
-          summaryByType.popConnectedFinalYear[f.elecTypeFinalYear] =
-            (summaryByType.popConnectedFinalYear[f.elecTypeFinalYear] || 0) +
-            parseFloat(f.popConnectedFinalYear);
-        }
+      // Renew cache expiration time
+      redisClient.expire(cacheKey, cacheTtl);
 
-        if (f.electrificationTech) {
-          // Investment for the target year
-          summaryByType.investmentCost[f.electrificationTech] =
-            (summaryByType.investmentCost[f.electrificationTech] || 0) +
-            parseFloat(f.investmentCost);
-
-          // Capacity for the target year
-          summaryByType.newCapacity[f.electrificationTech] =
-            (summaryByType.newCapacity[f.electrificationTech] || 0) +
-            parseFloat(f.newCapacity);
-        }
-      }
-
-      featureTypes = featureTypes.toString();
-
-      const response = { id, featureTypes, summary, summaryByType };
-
-      if (cache.enabled) {
-        // Store results in Redis
-        redisClient.setObject(cacheKey, response);
-      }
-
-      return response;
+      // Return results
+      return cachedData;
     } catch (error) {
       // Malformed queries
       if (error instanceof SyntaxError) {
---
+++ app/services/get-scenario.js
@@ -0,0 +1,280 @@
+const config = require('config');
+const qs = require('qs');
+const db = require('../db');
+const _ = require('lodash');
+
+// Create cache client only if Redis is available
+// Get Redis config
+const cache = config.get('cache');
+let redisClient;
+if (cache.enabled) {
+  redisClient = require('../redis-client');
+}
+
+module.exports = async function (id, query) {
+  let targetYear = null;
+  let filters;
+  const modelId = id.substring(0, id.lastIndexOf('-'));
+
+  // Generate cache key from query params
+  const cacheKey = JSON.stringify({ id, query });
+
+  // Set cache flag "runningDbQuery" as true
+  if (cache.enabled) {
+    await redisClient.setObject(
+      cacheKey,
+      { runningDbQuery: true },
+      cache.loadTimeout
+    );
+  }
+
+  if (query) {
+    query = qs.parse(query);
+    targetYear = parseInt(query.year) || null;
+
+    // Validate and parse filters
+    filters = query.filters;
+    if (filters) {
+      // Filters must be in an Array
+      if (!Array.isArray(filters)) {
+        throw new SyntaxError('Filters must be an Array.');
+      }
+
+      // Validate range values
+      filters.forEach(filter => {
+        // A filter key must be defined
+        if (typeof filter.key === 'undefined') {
+          throw new SyntaxError('Filter must include "key".');
+        }
+
+        if (
+          typeof filter.min === 'undefined' &&
+          typeof filter.max === 'undefined' &&
+          typeof filter.options === 'undefined'
+        ) {
+          throw new SyntaxError(
+            'Filter must include a valid value parameter name: "min", "max" or "options").'
+          );
+        }
+      });
+    }
+  }
+
+  // Get information about the model
+  const model = await db('models')
+    .select('filters', 'timesteps', 'baseYear')
+    .where('id', modelId)
+    .first();
+
+  const { baseYear } = model;
+  let [intermediateYear, finalYear] = model.timesteps;
+
+  // Validate timestep model
+  if (model.timesteps) {
+    // Set the year to the 1st value if null
+    targetYear = targetYear || model.timesteps[0];
+    if (model.timesteps.indexOf(targetYear) === -1) {
+      throw new SyntaxError(
+        `The "year" parameter [${targetYear}] is invalid for this scenario. Must be one of [${model.timesteps.join(
+          ', '
+        )}]`
+      );
+    }
+  } else {
+    // Disregard year
+    targetYear = '';
+  }
+
+  // Get steps before the target year. This is used to calculate accumulated
+  // investment costs
+  const includedSteps = model.timesteps.filter(y => y <= targetYear);
+  const investmentCostSelector = includedSteps
+    .map(year => {
+      return `(summary->>'InvestmentCost${year}')::numeric`;
+    })
+    .join(' + ');
+
+  // Assemble summary keys based on year numbers
+  const summaryKeys = {
+    popBaseYear: 'Pop' + baseYear,
+    popIntermediateYear: 'Pop' + intermediateYear,
+    popFinalYear: 'Pop' + finalYear,
+    popConnectedBaseYear: 'PopConnected' + baseYear,
+
+    elecStatusIntermediateYear: 'ElecStatusIn' + intermediateYear,
+    elecStatusFinalYear: 'ElecStatusIn' + finalYear,
+
+    elecTypeBaseYear: 'ElecCode' + baseYear,
+    elecTypeIntermediateYear: 'FinalElecCode' + intermediateYear,
+    elecTypeFinalYear: 'FinalElecCode' + finalYear,
+
+    electrificationTech: 'FinalElecCode' + targetYear,
+    newCapacity: 'NewCapacity' + targetYear,
+    electrificationStatus: 'ElecStatusIn' + targetYear
+  };
+
+  const whereBuilder = builder => {
+    builder.where('scenarioId', id);
+
+    if (filters) {
+      filters.forEach(filter => {
+        const { min, max, options } = filter;
+        let { key } = filter;
+
+        const filterDef = model.filters.find(f => f.key === key);
+        // Update key if is a timestep filter
+        key = filterDef.timestep ? key + targetYear : key;
+
+        if (typeof min !== 'undefined') {
+          builder.whereRaw(`("filterValues"->>?)::numeric >= ?`, [
+            key,
+            parseFloat(min)
+          ]);
+        }
+
+        if (typeof max !== 'undefined') {
+          builder.whereRaw(`("filterValues"->>?)::numeric <= ?`, [
+            key,
+            parseFloat(max)
+          ]);
+        }
+
+        if (Array.isArray(options) && options.length > 0) {
+          builder.whereRaw(
+            `("filterValues"->>?::text) in (${options
+              .map(_ => '?')
+              .join(',')})`,
+            [key, ...options]
+          );
+        }
+      });
+    }
+  };
+
+  // Get summary for filtered clusters
+  const summary = await db
+    .select(
+      db.raw(`
+        SUM(
+          (summary->>'${summaryKeys.popBaseYear}')::numeric
+        ) as "popBaseYear",
+        SUM(
+          (summary->>'${summaryKeys.popIntermediateYear}')::numeric
+        ) as "popIntermediateYear",
+        SUM(
+          (summary->>'${summaryKeys.popFinalYear}')::numeric
+          ) as "popFinalYear"
+      `),
+      db.raw(`
+        SUM(${investmentCostSelector}) as "investmentCost"
+      `),
+      db.raw(`
+        SUM(
+          (summary->>'${summaryKeys.newCapacity}')::numeric
+        ) as "newCapacity"
+      `)
+    )
+    .first()
+    .where(whereBuilder)
+    .from('scenarios');
+
+  // Parse query results into a 2 decimals number
+  Object.keys(summary).forEach(key => {
+    summary[key] = _.round(summary[key], 2);
+  });
+
+  // Get features
+  const features = await db
+    .select(
+      'featureId as id',
+      db.raw(
+        `summary->>'${summaryKeys.popConnectedBaseYear}' as "popConnectedBaseYear"`
+      ),
+      db.raw(
+        `(summary->>'${summaryKeys.popIntermediateYear}')::numeric * (summary->>'${summaryKeys.elecStatusIntermediateYear}')::numeric as "popConnectedIntermediateYear"`
+      ),
+      db.raw(
+        `(summary->>'${summaryKeys.popFinalYear}')::numeric * (summary->>'${summaryKeys.elecStatusFinalYear}')::numeric as "popConnectedFinalYear"`
+      ),
+      db.raw(
+        `summary->>'${summaryKeys.elecTypeBaseYear}' as "elecTypeBaseYear"`
+      ),
+      db.raw(
+        `summary->>'${summaryKeys.elecTypeIntermediateYear}' as "elecTypeIntermediateYear"`
+      ),
+      db.raw(
+        `summary->>'${summaryKeys.elecTypeFinalYear}' as "elecTypeFinalYear"`
+      ),
+      db.raw(
+        `summary->>'${summaryKeys.electrificationTech}' as "electrificationTech"`
+      ),
+      db.raw(
+        `(${investmentCostSelector}) as "investmentCost"`
+      ),
+      db.raw(`summary->>'${summaryKeys.newCapacity}' as "newCapacity"`),
+      db.raw(
+        `summary->>'${summaryKeys.electrificationStatus}' as "electrificationStatus"`
+      )
+    )
+    .where(whereBuilder)
+    .orderBy('featureId')
+    .from('scenarios');
+
+  const summaryByType = {
+    popConnectedBaseYear: {},
+    popConnectedIntermediateYear: {},
+    popConnectedFinalYear: {},
+    investmentCost: {},
+    newCapacity: {}
+  };
+
+  // Organize features into layers and calculate summary by type
+  let featureTypes = [];
+  for (const f of features) {
+    featureTypes[f.id] = f.electrificationTech;
+
+    // Base year, discard null electrification type
+    if (f.elecTypeBaseYear) {
+      summaryByType.popConnectedBaseYear[f.elecTypeBaseYear] =
+        (summaryByType.popConnectedBaseYear[f.elecTypeBaseYear] || 0) +
+        parseFloat(f.popConnectedBaseYear);
+    }
+
+    // Intermediate year
+    if (f.elecTypeIntermediateYear) {
+      summaryByType.popConnectedIntermediateYear[f.elecTypeIntermediateYear] =
+        (summaryByType.popConnectedIntermediateYear[f.elecTypeIntermediateYear] ||
+          0) + parseFloat(f.popConnectedIntermediateYear);
+    }
+
+    // Final year
+    if (f.elecTypeFinalYear) {
+      summaryByType.popConnectedFinalYear[f.elecTypeFinalYear] =
+        (summaryByType.popConnectedFinalYear[f.elecTypeFinalYear] || 0) +
+        parseFloat(f.popConnectedFinalYear);
+    }
+
+    if (f.electrificationTech) {
+      // Investment for the target year
+      summaryByType.investmentCost[f.electrificationTech] =
+        (summaryByType.investmentCost[f.electrificationTech] || 0) +
+        parseFloat(f.investmentCost);
+
+      // Capacity for the target year
+      summaryByType.newCapacity[f.electrificationTech] =
+        (summaryByType.newCapacity[f.electrificationTech] || 0) +
+        parseFloat(f.newCapacity);
+    }
+  }
+
+  featureTypes = featureTypes.toString();
+
+  const response = { id, featureTypes, summary, summaryByType };
+
+  // Cache query results
+  if (cache.enabled) {
+    redisClient.setObject(cacheKey, response);
+  }
+
+  return response;
+};
